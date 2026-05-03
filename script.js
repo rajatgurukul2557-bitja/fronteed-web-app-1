0617c87ac976f7cd859a72752338d20f6e33638b
rajatgurukul2557-bitja <rajat.gurukul2557@gmail.com>
2026-05-03 16:23:06 +0530

    Refactor quiz loading and submission logic

--- script.js
+++ script.js
@@ -1,84 +1,49 @@
-let quizData = [];
-let userAnswers = [];
-let timeLeft = 60;
-let timer;
+let questions = [];
 
-// Load quiz from backend
-async function loadQuiz() {
-    const res = await fetch("http://localhost:5000/api/quiz");
-    quizData = await res.json();
-
-    const quizDiv = document.getElementById("quiz");
-    quizDiv.innerHTML = "";
-
-    quizData.forEach((q, index) => {
-        let html = `<div class="question">
-        <h3>Q${index + 1}: ${q.question}</h3>`;
-
-        q.options.forEach(option => {
-            html += `
-            <label>
-                <input type="radio" name="q${index}" value="${option}" 
-                onchange="saveAnswer(${index}, '${option}')">
-                ${option}
-            </label><br>`;
-        });
-
-        html += `</div>`;
-        quizDiv.innerHTML += html;
+fetch('/get-questions')
+    .then(res => res.json())
+    .then(data => {
+        questions = data;
+        displayQuestions();
     });
 
-    startTimer();
+function displayQuestions() {
+    const quizDiv = document.getElementById("quiz");
+
+    questions.forEach((q, index) => {
+        const div = document.createElement("div");
+        div.classList.add("question");
+
+        div.innerHTML = `
+            <h3>${index + 1}. ${q.question}</h3>
+            ${q.options.map(opt => `
+                <label>
+                    <input type="radio" name="q${index}" value="${opt}">
+                    ${opt}
+                </label><br>
+            `).join("")}
+        `;
+
+        quizDiv.appendChild(div);
+    });
 }
 
-// Save selected answer
-function saveAnswer(index, answer) {
-    userAnswers[index] = answer;
-}
+function submitQuiz() {
+    const answers = questions.map((q, i) => {
+        const selected = document.querySelector(`input[name="q${i}"]:checked`);
+        return selected ? selected.value : "";
+    });
 
-// Timer logic
-function startTimer() {
-    timer = setInterval(() => {
-        timeLeft--;
-        document.getElementById("timer").innerText =
-            "Time Left: " + timeLeft;
-
-        if (timeLeft <= 0) {
-            clearInterval(timer);
-            submitQuiz();
-        }
-    }, 1000);
-}
-
-// Submit answers
-async function submitQuiz() {
-    if (userAnswers.length !== quizData.length) {
-        alert("Please answer all questions!");
-        return;
-    }
-
-    clearInterval(timer);
-
-    const res = await fetch("http://localhost:5000/api/submit", {
+    fetch('/submit', {
         method: "POST",
         headers: {
             "Content-Type": "application/json"
         },
-        body: JSON.stringify({ answers: userAnswers })
-    });
-
-    const data = await res.json();
-
-    document.getElementById("result").innerText =
-        `Score: ${data.score}/${data.total}`;
+        body: JSON.stringify({ answers })
+    })
+        .then(res => res.json())
+        .then(data => {
+            document.getElementById("result").innerText =
+                `Your Score: ${data.score} / ${data.total}`;
+        });
 }
-
-// Restart quiz
-function restartQuiz() {
-    userAnswers = [];
-    timeLeft = 60;
-    document.getElementById("result").innerText = "";
-    loadQuiz();
-}
-
-loadQuiz();
